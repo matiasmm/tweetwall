0405df3697aa44b6817b118e1719abe527b83fff
Ezequiel Pochiero <epochiero@gmail.com>
2012-09-25 23:31:30 -0300

    basic index

--- tweetwall/urls.py
+++ tweetwall/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns('',
-    url(r'^$', 'tweetwall.views.home', name='home'),
-
+    url(r'^tweetwall/$', TemplateView.as_view(template_name="index.html")),
+	url(r'^tweetwall/signin$', 'tweetwall.views.signin'),
 )
+
+urlpatterns += staticfiles_urlpatterns()
--- tweetwall/views.py
+++ tweetwall/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 *-*
 from django.http import HttpResponse
 
+
 def home(request):
     return HttpResponse("Hello, world. You're at the poll index.")
